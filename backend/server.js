25e1cf0e63f3fff4a05deb5c6652d6b9e30d3c2e
robertfalcone <robert.falcone@icrinc.com>
2025-02-27 11:23:39 -0500

    First commit

---
+++ backend/server.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sql = require('mssql');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 5002;
+
+// Middleware
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(cors());
+
+// Database configuration
+const dbConfig = {
+    user: 'xl_user', 
+    password: 'xl_4u',
+    server: 'STM-DEVDB01',
+    database: 'SCRATCH',
+    options: {
+      trustServerCertificate: true, // Trust self-signed certificate if needed
+      enableArithAbort: true,
+    },
+  };
+
+// API to get Ticker and Company Name
+app.get('/api/companies', async (req, res) => {
+    try {
+      let pool = await sql.connect(dbConfig);
+      let result = await pool.request().query(
+        'SELECT Ticker, CompanyName FROM icr.dbo.vwboardmembercompanylist ORDER BY CompanyName'
+      );
+      res.json(result.recordset);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error fetching data');
+    }
+  });
+  
+  // API to get Board Member Matrix Data
+  app.get('/api/board-member-matrix', async (req, res) => {
+    const { ticker, includeExecs } = req.query;
+    try {
+      let pool = await sql.connect(dbConfig);
+      let result = await pool
+        .request()
+        .input('ticker', sql.VarChar, ticker)
+        .input('includeExecs', sql.Bit, includeExecs === '1')
+        .execute('icr.dbo.spGetBoardMemberMatrixData');
+      res.json(result.recordset);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error fetching data');
+    }
+  });
+
+  // API to get Company Data
+  app.get('/api/company-data', async (req, res) => {
+    const { ticker, mostRecent } = req.query;
+    try {
+      let pool = await sql.connect(dbConfig);
+      let result = await pool
+        .request()
+        .input('ticker', sql.VarChar, ticker)
+        .input('MostRecent', sql.Bit, mostRecent === '1')
+        .query(`SELECT Name, Ticker, Title, Age, Sex, Compensation, Sector 
+          FROM ICR_BIGD.dbo.vwBoardMembers 
+          WHERE Ticker = @ticker AND MostRecent = @mostRecent`);
+      res.json(result.recordset);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error fetching data');
+    }
+  });
+
+  app.get('/api/get-old-board-members', async (req, res) => {
+    try {
+      let pool = await sql.connect(dbConfig);
+      let result = await pool
+        .request()
+        .execute('icr_bigd.dbo.spGetOldBoardMembers');
+      res.json(result.recordset);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error fetching data');
+    }
+  });
+
+  app.get('/api/get-new-client-boards', async (req, res) => {
+    try {
+      let pool = await sql.connect(dbConfig);
+      let result = await pool
+        .request()
+        .execute('icr_bigd.dbo.spGetClientNewBoards');
+      res.json(result.recordset);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error fetching data');
+    }
+  });
+
+  app.get('/api/get-long-term-board-members', async (req, res) => {
+    try {
+      let pool = await sql.connect(dbConfig);
+      let result = await pool
+        .request()
+        .execute('icr_bigd.dbo.spGetLongTermBoardMembers');
+      res.json(result.recordset);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error fetching data');
+    }
+  });
+
+  app.get('/api/get-board-high-turnover', async (req, res) => {
+    try {
+      let pool = await sql.connect(dbConfig);
+      let result = await pool
+        .request()
+        .execute('icr_bigd.dbo.spGetBoardHighTurnover');
+      res.json(result.recordset);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error fetching data');
+    }
+  });
+
+
+  app.listen(port, () => {
+    console.log('Server is running on port', port);
+  });
+  
---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,414 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import logo from './assets/ICR-logo.svg';
+import * as XLSX from 'xlsx';
+import ReactSelect from 'react-select';
+import { useMemo } from 'react';
+import { FixedSizeList as List } from "react-window";
+import React from "react";
+
+function App() {
+  const [companies, setCompanies] = useState([]);
+  const [matrixData, setMatrixData] = useState([]);
+  const [selectedTicker, setSelectedTicker] = useState('');
+  const [includeExecs, setIncludeExecs] = useState(false);
+  const [currentCompanyData, setCurrentCompanyData] = useState([]);
+  const [prevCompanyData, setPrevCompanyData] = useState([]);
+  const [allCompanies, setAllCompanies] = useState([]);
+  const [storedProcedureData, setStoredProcedureData] = useState(null);
+  const [currentProcedure, setCurrentProcedure] = useState('');
+
+  const port = 5002;
+
+  useEffect(() => {
+    fetch(`http://localhost:${port}/api/companies`)
+      .then((response) => response.json())
+      .then((data) => setCompanies(data))
+      .catch((error) => console.error('Error fetching companies:', error));
+  }, []);
+
+  const fetchMatrixData = () => {
+    fetch(
+      `http://localhost:${port}/api/board-member-matrix?ticker=${selectedTicker}&includeExecs=${includeExecs ? 1 : 0}`
+    )
+      .then((response) => response.json())
+      .then((data) => fetchTransformedData(data))
+      .catch((error) => console.error('Error fetching matrix data:', error));
+    setCurrentCompanyData([]);
+    setPrevCompanyData([]);
+    setStoredProcedureData([]);
+    setCurrentProcedure("");
+  };
+
+  const fetchCurrentCompanyData = () => {
+    fetch(`http://localhost:${port}/api/company-data?ticker=${selectedTicker}&mostRecent=1`)
+      .then((response) => response.json())
+      .then((data) => setCurrentCompanyData(data))
+      .catch((error) => console.error('Error fetching companies:', error));
+    setMatrixData([]);
+    setStoredProcedureData([]);
+    setCurrentProcedure("");
+  };
+
+  const fetchPrevCompanyData = () => {
+    fetch(`http://localhost:${port}/api/company-data?ticker=${selectedTicker}&mostRecent=0`)
+      .then((response) => response.json())
+      .then((data) => setPrevCompanyData(data))
+      .catch((error) => console.error('Error fetching companies:', error));
+    setMatrixData([]);
+    setStoredProcedureData([]);
+    setCurrentProcedure("");
+  };
+
+  const fetchAllCompanyData = () => {
+    fetchCurrentCompanyData();
+    fetchPrevCompanyData();
+  };
+
+  const fetchTransformedData = (data) => {
+    if (selectedTicker) {
+      const memberCompanyMap = {};
+
+      data.forEach((row) => {
+        if (!memberCompanyMap[row.Name]) {
+          memberCompanyMap[row.Name] = [];
+        }
+        memberCompanyMap[row.Name].push({
+          CompanyName: row.CompanyName,
+          Title: row.Title,
+        });
+      });
+
+      const overlappingMembers = Object.keys(memberCompanyMap).filter(
+        (name) => memberCompanyMap[name].length > 1
+      );
+
+      const overlappingMatrix = overlappingMembers.map((name) => {
+        const companies = memberCompanyMap[name];
+        return {
+          Name: name,
+          Companies: companies,
+        };
+      });
+
+      setMatrixData(overlappingMatrix);
+    }
+  };
+
+  useEffect(() => {
+    if (matrixData.length > 0) {
+      const uniqueCompanies = Array.from(
+        new Set(matrixData.flatMap(person => person.Companies.map(c => c.CompanyName)))
+      );
+      setAllCompanies(uniqueCompanies);
+    }
+  }, [matrixData]);
+
+  const executeStoredProcedure = async (procedureName) => {
+    try {
+      const response = await fetch(
+        `http://localhost:${port}/api/${procedureName}`
+      );
+      const data = await response.json();
+      setStoredProcedureData(data);
+    } catch (error) {
+      console.error("Error executing stored procedure:", error);
+    }
+    setMatrixData([]);
+    setCurrentCompanyData([]);
+    setPrevCompanyData([]);
+    if (procedureName === "get-old-board-members") {
+      setCurrentProcedure("Old Board Members");
+    }
+    if (procedureName === "get-new-client-boards") {
+      setCurrentProcedure("Client New Boards");
+    }
+    if (procedureName === "get-long-term-board-members") {
+      setCurrentProcedure("Long Term Board Members");
+    }
+    if (procedureName === "get-board-high-turnover") {
+      setCurrentProcedure("Board High Turnover");
+    }
+  };
+
+  const options = useMemo(() => {
+    return companies.map(company => ({
+      value: company.Ticker,
+      label: company.CompanyName
+    }));
+  }, [companies]);
+
+  const customMenuList = (props) => {
+    const { children, maxHeight } = props;
+    const itemHeight = 50;
+    const itemCount = React.Children.count(children);
+  
+    return (
+      <List
+        height={Math.min(maxHeight, itemCount * itemHeight)}
+        itemCount={itemCount}
+        itemSize={itemHeight}
+        width="100%"
+      >
+        {({ index, style }) => (
+          <div style={style}>{children[index]}</div>
+        )}
+      </List>
+    );
+  };
+
+  const exportToExcel = () => {
+    const data = [];
+    let fileName = '';
+  
+    if (storedProcedureData.length > 0) {
+      fileName = currentProcedure.replace(/ /g, '_');
+      const header = Object.keys(storedProcedureData[0]);
+      const rows = storedProcedureData.map(row => Object.values(row));
+      data.push([header, ...rows]);
+    }
+  
+    if (matrixData.length > 0) {
+      fileName = `${selectedTicker}_Overlapping_Board_Members`;
+      const header = ["Name", ...allCompanies];
+      const rows = matrixData.map(row => [
+        row.Name,
+        ...allCompanies.map((company) => {
+          const companyEntry = row.Companies.find(c => c.CompanyName === company);
+          return companyEntry ? companyEntry.Title : '';
+        }),
+      ]);
+      data.push([header, ...rows]);
+    }
+  
+    if (currentCompanyData.length > 0) {
+      fileName = `${selectedTicker}_Current_Company_Data`;
+      const header = ["Name", "Ticker", "Title", "Age", "Sex", "Compensation", "Sector"];
+      const rows = currentCompanyData.map(row => [
+        row.Name,
+        row.Ticker,
+        row.Title,
+        row.Age === 0 ? "Unknown" : row.Age,
+        row.Sex,
+        row.Compensation ? `$${new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(row.Compensation)}` : '-',
+        row.Sector,
+      ]);
+      data.push([header, ...rows]);
+    }
+
+    if (prevCompanyData.length > 0) {
+      fileName = `${selectedTicker}_Previous_Company_Data`;
+      const header = ["Name", "Ticker", "Title", "Age", "Sex", "Compensation", "Sector"];
+      const rows = prevCompanyData.map(row => [
+        row.Name,
+        row.Ticker,
+        row.Title,
+        row.Age === 0 ? "Unknown" : row.Age,
+        row.Sex,
+        row.Compensation ? `$${new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(row.Compensation)}` : '-',
+        row.Sector,
+      ]);
+      data.push([header, ...rows]);
+    }
+
+    if (currentCompanyData.length > 0 && prevCompanyData.length > 0) {
+      fileName = `${selectedTicker}_Curr_Prev_Company_Data`;
+    }
+  
+    const ws = XLSX.utils.aoa_to_sheet(data.flat());
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, fileName);
+    XLSX.writeFile(wb, `${fileName}.xlsx`);
+  };
+
+  return (
+    <div className="flex">
+      <div className="main-container">
+        <div className="button-panel">
+          <div style={{display: 'flex', flexDirection: 'row'}}>
+            <img src={logo} style={{ width: "100px", alignContent: "center" }} alt="Logo" />
+            <p style={{marginLeft: "5px"}}><b>Instructions:</b> Enter the fields below to use the first two buttons. </p>
+          </div>
+          <div className="searchable-select">
+            <ReactSelect
+              options={options}
+              onChange={(selectedOption) => setSelectedTicker(selectedOption.value)}
+              placeholder="Select a company"
+              components={{ MenuList: customMenuList }}
+            />
+          </div>
+  
+          <label className="flex items-center space-x-2">
+            <input
+              type="checkbox"
+              checked={includeExecs}
+              onChange={(e) => setIncludeExecs(e.target.checked)}
+            />
+            <span>Include Executives</span>
+          </label>
+          <button onClick={fetchMatrixData} className="button w-full">Fetch Overlapping Board Members</button>
+          <button onClick={fetchAllCompanyData} className="button w-full">Fetch Company Board Members</button>
+          <hr className='divider'/>
+          <button onClick={() => executeStoredProcedure("get-old-board-members")} className="button w-full">Get Old Board Members</button>
+          <button onClick={() => executeStoredProcedure("get-new-client-boards")} className="button w-full">Get Client New Boards</button>
+          <button onClick={() => executeStoredProcedure("get-long-term-board-members")} className="button w-full">Get Long Term Board Members</button>
+          <button onClick={() => executeStoredProcedure("get-board-high-turnover")} className="button w-full">Get Board High Turnover</button>
+          <button onClick={exportToExcel} className="excel-button">Export to Excel</button>
+        </div>
+  
+        <div className="all-tables">
+          {storedProcedureData && (
+                <div className="table-container">
+                  <h3><u>{currentProcedure}</u></h3>
+                  <table>
+                    <thead>
+                      <tr>
+                        {Object.keys(storedProcedureData[0] || {}).map((key) => (
+                          <th key={key}>{key}</th>
+                        ))}
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {storedProcedureData.map((row, index) => (
+                      <tr key={index}>
+                        {Object.entries(row).map(([key, value], i) => (
+                          <td key={i}>
+                            {key.toLowerCase() === "compensation" && value
+                              ? `$${new Intl.NumberFormat('en-US', {
+                                  minimumFractionDigits: 2,
+                                  maximumFractionDigits: 2
+                                }).format(value)}`
+                              : (key.toLowerCase().includes("_date") || 
+                              (key.toLowerCase().includes("addeddt2")) || 
+                              (key.toLowerCase().includes("lastturnoverreportdate")) && value
+                                  ? new Date(value).toLocaleDateString('en-US')
+                                  : value)}
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+                    </tbody>
+                  </table>
+                </div>
+              )}
+
+              {matrixData.length > 0 && (
+                <div className='table-container'>
+                  <h3><u>Overlapping Board Members</u></h3>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        {allCompanies.map((company, index) => (
+                          <th key={index}>{company}</th>
+                        ))}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {matrixData.map((row, rowIndex) => (
+                      <tr key={rowIndex}>
+                        <td>
+                          <div className="container">
+                            <span className="trigger">{row.Name}</span>
+                            <span className="tooltip">
+                              <b>{row.Name}</b>
+                              <br />
+                              <b>Tickers: </b>
+                              {[
+                              ...row.Companies
+                                .map((companyEntry) => {
+                                  const matchingCompany = companies.find(c => c.CompanyName === companyEntry.CompanyName);
+                                  return matchingCompany ? matchingCompany.Ticker : null;
+                                })
+                                .filter(Boolean),
+                              selectedTicker
+                            ]
+                              .filter(Boolean)
+                              .join(", ")}
+                            </span>
+                          </div>
+                        </td>
+                        {allCompanies.map((company, colIndex) => {
+                          const companyEntry = row.Companies.find(c => c.CompanyName === company);
+                          return (
+                            <td key={colIndex}>
+                              {companyEntry ? companyEntry.Title : ""}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    ))}
+                    </tbody>
+                  </table>
+                </div>
+              )}
+
+              {currentCompanyData.length > 0 && (
+                <div className='table-container'>
+                  <h3><u>Current Board Members</u></h3>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Ticker</th>
+                        <th>Title</th>
+                        <th>Age</th>
+                        <th>Sex</th>
+                        <th>Compensation</th>
+                        <th>Sector</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {currentCompanyData.map((row, index) => (
+                        <tr key={index}>
+                          <td>{row.Name}</td>
+                          <td>{row.Ticker}</td>
+                          <td>{row.Title}</td>
+                          <td>{row.Age === 0 ? "Unknown" : row.Age}</td>
+                          <td>{row.Sex ? row.Sex.charAt(0).toUpperCase() + row.Sex.slice(1).toLowerCase() : ''}</td>
+                          <td>{row.Compensation ? `$${new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(row.Compensation)}` : '-'}</td>
+                          <td>{row.Sector}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  {prevCompanyData.length > 0 && ( 
+                    <>
+                      <h3><u>Previous Board Members</u></h3>
+                      <table>
+                        <thead>
+                          <tr>
+                            <th>Name</th>
+                            <th>Ticker</th>
+                            <th>Title</th>
+                            <th>Age</th>
+                            <th>Sex</th>
+                            <th>Compensation</th>
+                            <th>Sector</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {prevCompanyData.map((row, index) => (
+                            <tr key={index}>
+                              <td>{row.Name}</td>
+                              <td>{row.Ticker}</td>
+                              <td>{row.Title}</td>
+                              <td>{row.Age}</td>
+                              <td>{row.Sex}</td>
+                              <td>{row.Compensation ? `$${new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(row.Compensation)}` : '-'}</td>
+                              <td>{row.Sector}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </>
+                  )}
+                </div>
+              )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
